12e938a67134c85721b5be75a5c787bede9fec2d
Gemma <g.ragadini@studenti.unipi.it>
2022-11-04 17:39:11 +0100

    adding one more file

--- generatorPath.hpp
+++ generatorPath.hpp
@@ -1,35 +1,35 @@
-#pragma once
+// #pragma once
 
-#include <iostream>
-#include <stdlib.h>
-#include <vector>
-#include <iostream>
-#include <math.h>
-#include <CGAL/Point_2.h>
-#include <CGAL/Polygon_2.h>
-#include <CGAL/Segment_2.h>
-#include <CGAL/Vector_2.h>
-#include <CGAL/Direction_2.h>
-#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
-#include <CGAL/Simple_cartesian.h>
-#include <CGAL/squared_distance_2.h>
-#include <CGAL/intersections.h>
-#include <CGAL/Object.h>
-#include "utils.hpp"
+// #include <iostream>
+// #include <stdlib.h>
+// #include <vector>
+// #include <iostream>
+// #include <math.h>
+// #include <CGAL/Point_2.h>
+// #include <CGAL/Polygon_2.h>
+// #include <CGAL/Segment_2.h>
+// #include <CGAL/Vector_2.h>
+// #include <CGAL/Direction_2.h>
+// #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+// #include <CGAL/Simple_cartesian.h>
+// #include <CGAL/squared_distance_2.h>
+// #include <CGAL/intersections.h>
+// #include <CGAL/Object.h>
+// #include "utils.hpp"
 
-typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
-typedef K::Point_2 Point;
-typedef CGAL::Polygon_2<K> Polygon;
-typedef CGAL::Segment_2<K> Segment;
-typedef CGAL::Line_2<K> Line;
-typedef CGAL::Vector_2<K> Vector;
-typedef CGAL::Direction_2<K> Direction;
+// typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
+// typedef K::Point_2 Point;
+// typedef CGAL::Polygon_2<K> Polygon;
+// typedef CGAL::Segment_2<K> Segment;
+// typedef CGAL::Line_2<K> Line;
+// typedef CGAL::Vector_2<K> Vector;
+// typedef CGAL::Direction_2<K> Direction;
 
-using namespace std;
+// using namespace std;
 
-float angularCoeff(Segment segment);
-float qCoeff(Segment segment, float angular_coeff);
-// Point* findPointsAtDistance(float m, float q, Point a, float dx);
-// Point choosePoint (Point points[2], Point target);
-vector<Point> divideSegment(Segment segment, float range);
-void generatePath(Polygon poly, vector<Point>& path);
+// float angularCoeff(Segment segment);
+// float qCoeff(Segment segment, float angular_coeff);
+// // Point* findPointsAtDistance(float m, float q, Point a, float dx);
+// // Point choosePoint (Point points[2], Point target);
+// vector<Point> divideSegment(Segment segment, float sweep_distance);
+// void generatePath(shared_ptr<Polygon> poly, vector<Point>& path);
