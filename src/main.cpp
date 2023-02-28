7bdd25e5c2f3693c209f390eb4ea524389e90532
trului <luigi.truppa@aitronik.com>
2023-02-28 10:58:33 +0100

    restore main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+#include <iostream>
+#include <vector>
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/partition_2.h>
+#include <CGAL/Partition_traits_2.h>
+#include "utils.hpp"
+#include "CoveragePathCreator.h"
+
+//deve funzionare con sweepdistance 0? 
+//sistemare gli shared ptr
+//quando non ci sono intersezioni prendi il centro invece che il primo  vertice
+//manca una strisciata a volte ==> riguardare come trova il punto a max distanza, forse non è divideSegment che si perde un punto, ma proprio dall'inizio 
+
+//  ./coverage_path ../test/mydecompositiontest2.txt 1 2
+// ./coverage_path ../test/a.txt 1 1
+// perimeter.txt", "1", "10" ==> nella decomposizione c'è un poligono strano
+// perimeter.txt", "1", "10" , sto provando a non farlo crashare ma bisogna completare ( vedi questo esempio )
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    
+    //argomenti linea di comando
+    if (argc < 4) { 
+        cout << "Argument Error" << endl;
+        return 1;
+    }
+    string filename = argv[1];
+    
+    //leggo input 
+    vector<pair<float,float>> points = readFromFile(filename);
+
+
+    //inizializzazione Coverage Path Creator 
+    CoveragePathCreator coverage_path_creator;
+    if (!coverage_path_creator.init(points, stof(argv[3]) , stoi(argv[2]))) {
+        cout << "CoveragePathCreator: Errore inizializzazione" << endl;
+        return 1;
+    } 
+
+    coverage_path_creator.run();
+
+    points = coverage_path_creator.getFinalPath(); 
+
+    return 0;
+}
